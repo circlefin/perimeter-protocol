e00aa97bbcc134b47adc33487dd78b7535223e63
Matt Venables <matt@venabl.es>
2022-09-08 10:33:39 -0400

    Add eslint, prettier for formatting

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Lock {
-    uint public unlockTime;
+    uint256 public unlockTime;
     address payable public owner;
 
-    event Withdrawal(uint amount, uint when);
+    event Withdrawal(uint256 amount, uint256 when);
 
-    constructor(uint _unlockTime) payable {
+    constructor(uint256 _unlockTime) payable {
         require(
             block.timestamp < _unlockTime,
             "Unlock time should be in the future"
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,8 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16"
-};
+}
 
-export default config;
+export default config
