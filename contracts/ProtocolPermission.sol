ec21f306cf717774e736f1affb1fd38a1acd3c01
Matt Venables <matt@venabl.es>
2022-09-15 09:46:35 -0400

    Update ProtocolPermission to have singular setAllowed method

--- contracts/ProtocolPermission.sol
+++ contracts/ProtocolPermission.sol
@@ -16,32 +16,21 @@ contract ProtocolPermission is Ownable, IProtocolPermission {
     event AllowListUpdated(address indexed addr, bool isAllowed);
 
     /**
-     * @dev Adds an address to the allowList.
-     * @param addr The address to add to the allowList
-     *
-     * Emits an {AllowListAdded} event.
-     */
-    function addToAllowList(address addr) external onlyOwner {
-        _allowList[addr] = true;
-        emit AllowListUpdated(addr, true);
-    }
-
-    /**
-     * @dev Removes an address from the allowList.
-     * @param addr The address to remove from the allowList
-     *
-     * Emits an {AllowListRemoved} event.
-     */
-    function removeFromAllowList(address addr) external onlyOwner {
-        delete _allowList[addr];
-        emit AllowListUpdated(addr, false);
-    }
-
-    /**
      * @dev Checks against an allowList to see if the given address is allowed.
      * See {IProtocolPermission-isAllowed}.
      */
     function isAllowed(address addr) external view returns (bool) {
         return _allowList[addr];
+    }
+
+    /**
+     * @dev Adds or removes an address from the allowList.
+     * @param addr The address to add or remove
+     *
+     * Emits an {AllowListUpdated} event.
+     */
+    function setAllowed(address addr, bool allow) external onlyOwner {
+        _allowList[addr] = allow;
+        emit AllowListUpdated(addr, allow);
     }
 }
