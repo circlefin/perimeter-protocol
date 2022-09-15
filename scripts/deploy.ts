103b7a6a936cef9d0ec8a2d86b2150e8f898c77b
Matt Venables <matt@venabl.es>
2022-09-15 10:23:36 -0400

    Update deploy script to use ProtocolPermission

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const ProtocolPermission = await ethers.getContractFactory(
+    "ProtocolPermission"
   );
+  const protocolPermission = await ProtocolPermission.deploy();
+
+  await protocolPermission.deployed();
+
+  console.log(`ProtocolPermission deployed to ${protocolPermission.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
