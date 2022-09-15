78ebce59afb6d075d09061985cd2dd2ef952fa22
Matt Venables <matt@venabl.es>
2022-09-15 10:52:00 -0400

    Add basic titles to interface and contracts

--- contracts/interfaces/IProtocolPermission.sol
+++ contracts/interfaces/IProtocolPermission.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.16;
 
+/**
+ * @title The most basic interface that a Protocol Permission contract must
+ * implement.
+ */
 interface IProtocolPermission {
     /**
      * @dev Check if an address is allowed as a Pool Manager
