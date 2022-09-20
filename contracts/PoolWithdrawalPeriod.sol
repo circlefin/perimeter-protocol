48a9e555d2cea58ec9fe3efd9fe69b0235f48605
ams9198 <111915188+ams9198@users.noreply.github.com>
2022-09-20 14:23:15 -0400

    Add ERC-4246 and IPool interfaces (#11)

---
+++ contracts/PoolWithdrawalPeriod.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+/**
+ * @dev Contains the start and enddate of a given withdrawal period.
+ */
+struct PoolWithdrawalPeriod {
+    uint256 start;
+    uint256 end;
+}
---
+++ contracts/interfaces/IERC4246.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title The interface according to the ERC-4626 standard.
+ */
+interface IERC4626 is IERC20 {
+    /**
+     * @dev Emitted when tokens are deposited into the vault via the mint and deposit methods.
+     */
+    event Deposit(
+        address indexed caller,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+
+    /**
+     * @dev Emitted when shares are withdrawn from the vault by a depositor in the redeem or withdraw methods.
+     */
+    event Withdraw(
+        address indexed caller,
+        address indexed receiver,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+
+    /**
+     * @dev Return the address of the underlying ERC-20 token used for the vault for accounting, depositing, withdrawing.
+     */
+    function asset() external view returns (address);
+
+    /**
+     * @dev Calculate the total amount of underlying assets held by the vault.
+     */
+    function totalAssets() external view returns (uint256);
+
+    /**
+     * @dev Calculates the amount of shares that would be exchanged by the vault for the amount of assets provided.
+     */
+    function convertToShares(uint256 assets) external view returns (uint256);
+
+    /**
+     * @dev Calculates the amount of assets that would be exchanged by the vault for the amount of shares provided.
+     */
+    function convertToAssets(uint256 shares) external view returns (uint256);
+
+    /**
+     * @dev Calculates the maximum amount of underlying assets that can be deposited in a single deposit call by the receiver.
+     */
+    function maxDeposit(address receiver) external view returns (uint256);
+
+    /**
+     * @dev Allows users to simulate the effects of their deposit at the current block.
+     */
+    function previewDeposit(uint256 assets) external view returns (uint256);
+
+    /**
+     * @dev Deposits assets of underlying tokens into the vault and grants ownership of shares to receiver.
+     * Emits a {Deposit} event.
+     */
+    function deposit(uint256 assets, address receiver)
+        external
+        returns (uint256);
+
+    /**
+     * @dev Returns the maximum amount of shares that can be minted in a single mint call by the receiver.
+     */
+    function maxMint(address receiver) external view returns (uint256);
+
+    /**
+     * @dev Allows users to simulate the effects of their mint at the current block.
+     */
+    function previewMint(uint256 shares) external view returns (uint256);
+
+    /**
+     * @dev Mints exactly shares vault shares to receiver by depositing assets of underlying tokens.
+     * Emits a {Deposit} event.
+     */
+    function mint(uint256 shares, address receiver) external returns (uint256);
+
+    /**
+     * @dev Returns the maximum amount of underlying assets that can be withdrawn from the owner balance with a single withdraw call.
+     */
+    function maxWithdraw(address owner) external view returns (uint256);
+
+    /**
+     * @dev Simulate the effects of their withdrawal at the current block.
+     */
+    function previewWithdraw(uint256 assets) external view returns (uint256);
+
+    /**
+     * @dev Burns shares from owner and send exactly assets token from the vault to receiver.
+     * Emits a {Withdraw} event.
+     */
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) external returns (uint256);
+
+    /**
+     * @dev The maximum amount of shares that can be redeemed from the owner balance through a redeem call.
+     */
+    function maxRedeem(address owner) external view returns (uint256);
+
+    /**
+     * @dev Simulates the effects of their redeemption at the current block.
+     */
+    function previewRedeem(uint256 shares) external view returns (uint256);
+
+    /**
+     * @dev Redeems a specific number of shares from owner and send assets of underlying token from the vault to receiver.
+     * Emits a {Withdraw} event.
+     */
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) external returns (uint256);
+}
