a9f78c6264a94aec09ec1456ed616a7532981a5f
Brice Stacey <brice@m2.xyz>
2022-09-20 11:18:09 -0400

    Add basic PoolFactory

---
+++ contracts/Pool.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+/**
+ * @title Pool
+ *
+ * Empty Pool contract.
+ */
+contract Pool {
+}
---
+++ test/PoolFactory.test.ts
@@ -0,0 +1,22 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("PoolFactory", () => {
+  async function deployFixture() {
+    const PoolFactory = await ethers.getContractFactory("PoolFactory");
+    const poolFactory = await PoolFactory.deploy();
+    await poolFactory.deployed();
+
+    return {
+      poolFactory
+    };
+  }
+
+  it("emits PoolCreated", async () => {
+    const { poolFactory } = await loadFixture(deployFixture);
+    await expect(poolFactory.createPool())
+      .to.emit(poolFactory, "PoolCreated");
+  });
+
+});
