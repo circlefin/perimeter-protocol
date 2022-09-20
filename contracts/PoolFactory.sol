d7b76616f0c208bfac59ea83a3b654264da09410
Brice Stacey <brice@m2.xyz>
2022-09-20 11:54:26 -0400

    Add basic PoolFactory (#15)

---
+++ contracts/PoolFactory.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import "./Pool.sol";
+
+/**
+ * @title PoolFactory
+ */
+contract PoolFactory {
+
+    /**
+     * @dev Emitted when a pool is created.
+     */
+    event PoolCreated(address indexed addr);
+
+    /**
+     * @dev Creates a pool
+     * @dev Emits `PoolCreated` event.
+     */
+    function createPool() external returns (address poolAddress) {
+        Pool pool = new Pool();
+        address addr = address(pool);
+        emit PoolCreated(addr);
+        return addr;
+    }
+
+}
