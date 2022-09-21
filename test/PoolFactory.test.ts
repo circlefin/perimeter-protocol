03ad49ecce6f17cfc0113b5045cc6644e0ed36d5
Adrian Soghoian <adrian.soghoian@contractor.circle.com>
2022-09-21 12:24:33 -0400

    Simplify factory test.

--- test/PoolFactory.test.ts
+++ test/PoolFactory.test.ts
@@ -1,10 +1,10 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("PoolFactory", () => {
 
-  const MOCK_LIQUIDITY_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const MOCK_LIQUIDITY_ADDRESS = "0x0000000000000000000000000000000000000001";
 
   async function deployFixture() {
     const PoolFactory = await ethers.getContractFactory("PoolFactory");
@@ -21,8 +21,8 @@ describe("PoolFactory", () => {
 
     await expect(poolFactory.createPool(
       MOCK_LIQUIDITY_ADDRESS,
-      1_000_000,
-      (await time.latest()) + (60 * 60),
+      0,
+      0,
       0
     ))
       .to.emit(poolFactory, "PoolCreated");
