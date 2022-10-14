d24d7d9a67503381696238ba419ee9a228a6a97d
Brice Stacey <brice@m2.xyz>
2022-10-14 14:13:00 -0400

    Add FeeVault

---
+++ contracts/FeeVault.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IPool} from "./interfaces/IPool.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract FeeVault {
+    using SafeERC20 for IERC20;
+
+    address public immutable pool;
+
+    modifier onlyPoolAdmin() {
+        require(
+            msg.sender == IPool(pool).manager(),
+            "FeeVault: caller not pool admin"
+        );
+        _;
+    }
+
+    constructor(address pool_) {
+        pool = pool_;
+    }
+
+    /**
+     * @dev Allows withdrawal of fees held by vault.
+     */
+    function withdraw(address asset, uint256 amount) external onlyPoolAdmin {
+        IERC20(asset).safeTransfer(msg.sender, amount);
+    }
+}
