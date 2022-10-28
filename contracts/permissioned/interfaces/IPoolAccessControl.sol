8377b5e31cb13096ff86c346699806ffb1b7e4e2
Matt Venables <matt@venabl.es>
2022-10-28 16:08:33 -0400

    Add verite credential support (#69)
    * Add verite credential support
    
    * Remove ToS check on isValidLender
    
    We check whenever adding an address, so we do not need to use the cycles
    checking here every time

--- contracts/permissioned/interfaces/IPoolAccessControl.sol
+++ contracts/permissioned/interfaces/IPoolAccessControl.sol
@@ -12,3 +12,13 @@ interface IPoolAccessControl {
      */
     function isValidLender(address addr) external view returns (bool);
 }
+
+/**
+ * @dev Verite credentials will submit a verification result in this format.
+ */
+struct VerificationResult {
+    string schema; // indicator of the type of verification result
+    address subject; // address of the subject of the verification
+    uint256 expiration; // expiration of verification (may or may not be expiration of the VC)
+    string verifier_verification_id; // Unique ID from the verifier
+}
