a7a03d9c7aea8d1ebd3fea9ad3e1bdd42e5d1063
Adrian Soghoian <adrian.soghoian@contractor.circle.com>
2022-11-03 14:50:45 -0400

    Cleanups

--- test/scenarios/business/2.test.ts
+++ test/scenarios/business/2.test.ts
@@ -6,240 +6,241 @@ import { collateralizeLoan, deployLoan, fundLoan } from "../../support/loan";
 import { deployMockERC20 } from "../../support/erc20";
 
 describe("Business Scenario 2", () => {
-    const INPUTS = {
-        lenderADepositAmount: 500_000_000_000, // $500k in USDC
-        lenderBDepositAmount: 500_000_000_000, // $500k in USDC
-        pool: {
-            maxCapacity: 20_000_000_000_000, // $20M in USDC
-            requestFeeBps: 500, // 5%
-            withdrawGateBps: 5_000, // 50%
-            requestCancellationFeeBps: 0,
-            firstLossInitialMinimum: 0,
-            withdrawRequestPeriodDuration: 7 * 24 * 60 * 60, // 7 days
-            fixedFee: 0,
-            fixedFeeInterval: 0,
-            poolFeePercentOfInterest: 2_000 // 20%
-        },
-        loan: {
-            duration: 14,
-            paymentPeriod: 7,
-            loanType: 0,
-            apr: 1000,
-            principal: 1_000_000_000_000,
-            dropDeadDateDuration: 0,
-            latePaymentFee: 0,
-            latePayment: 0,
-            originationBps: 0
-        },
-        loanPayment: 3888_890_000 // each loan payment, + rounding
+  const INPUTS = {
+    lenderADepositAmount: 500_000_000_000, // $500k in USDC
+    lenderBDepositAmount: 500_000_000_000, // $500k in USDC
+    pool: {
+      maxCapacity: 20_000_000_000_000, // $20M in USDC
+      requestFeeBps: 500, // 5%
+      withdrawGateBps: 5_000, // 50%
+      requestCancellationFeeBps: 0,
+      firstLossInitialMinimum: 0,
+      withdrawRequestPeriodDuration: 7 * 24 * 60 * 60, // 7 days
+      fixedFee: 0,
+      fixedFeeInterval: 0,
+      poolFeePercentOfInterest: 2_000 // 20%
+    },
+    loan: {
+      duration: 14,
+      paymentPeriod: 7,
+      loanType: 0,
+      apr: 1000,
+      principal: 1_000_000_000_000,
+      dropDeadDateDuration: 0,
+      latePaymentFee: 0,
+      latePayment: 0,
+      originationBps: 0
+    },
+    loanPayment: 3888_890_000 // each loan payment, + rounding
+  };
+
+  async function advanceToDay(startTime: number, numberDays: number) {
+    await time.increaseTo(startTime + numberDays * 3600 * 24);
+  }
+
+  async function fixtures() {
+    const [poolManager, lenderA, lenderB, borrower] = await ethers.getSigners();
+
+    const endTime = (await time.latest()) + 5_184_000; // 60 days.
+    const poolSettings = {
+      endDate: endTime, // Jan 1, 2050
+      ...INPUTS.pool
     };
+    const { mockERC20: mockUSDC } = await deployMockERC20(
+      "Mock USDC",
+      "MUSDC",
+      6
+    );
+    const { pool, serviceConfiguration } = await deployPool(
+      poolManager,
+      poolSettings,
+      mockUSDC
+    );
 
-    async function advanceToDay(startTime: number, numberDays: number) {
-        await time.increaseTo(startTime + numberDays * 3600 * 24);
-    }
+    // Confirm FL fee is set to 5%
+    expect(await serviceConfiguration.firstLossFeeBps()).to.equal(500);
 
-    async function fixtures() {
-        const [poolManager, lenderA, lenderB, borrower] = await ethers.getSigners();
+    // activate pool
+    await activatePool(pool, poolManager, mockUSDC);
+    const startTime = (await pool.poolActivatedAt()).toNumber();
 
-        const endTime = (await time.latest()) + 5_184_000; // 60 days.
-        const poolSettings = {
-            endDate: endTime, // Jan 1, 2050
-            ...INPUTS.pool
-        };
-        const { mockERC20: mockUSDC } = await deployMockERC20(
-            "Mock USDC",
-            "MUSDC",
-            6
-        );
-        const { pool, serviceConfiguration } = await deployPool(
-            poolManager,
-            poolSettings,
-            mockUSDC
-        );
+    // Mint for lenders
+    await mockUSDC.mint(lenderA.address, INPUTS.lenderADepositAmount);
+    await mockUSDC.mint(lenderB.address, INPUTS.lenderBDepositAmount);
 
-        // Confirm FL fee is set to 5%
-        expect(await serviceConfiguration.firstLossFeeBps()).to.equal(500);
+    // Lender allowance for pool
+    await mockUSDC
+      .connect(lenderA)
+      .approve(pool.address, INPUTS.lenderADepositAmount);
+    await mockUSDC
+      .connect(lenderB)
+      .approve(pool.address, INPUTS.lenderBDepositAmount);
 
-        // activate pool
-        await activatePool(pool, poolManager, mockUSDC);
-        const startTime = (await pool.poolActivatedAt()).toNumber();
+    // Create loanOne
+    const { loan } = await deployLoan(
+      pool.address,
+      borrower.address,
+      mockUSDC.address,
+      serviceConfiguration,
+      INPUTS.loan
+    );
+    // mint USDC for borrower to pay down loanOne
+    await mockUSDC.mint(borrower.address, INPUTS.loanPayment);
 
-        // Mint for lenders
-        await mockUSDC.mint(lenderA.address, INPUTS.lenderADepositAmount);
-        await mockUSDC.mint(lenderB.address, INPUTS.lenderBDepositAmount);
+    // Collateralize loan
+    await collateralizeLoan(loan, borrower, mockUSDC, 0);
 
-        // Lender allowance for pool
-        await mockUSDC
-            .connect(lenderA)
-            .approve(pool.address, INPUTS.lenderADepositAmount);
-        await mockUSDC
-            .connect(lenderB)
-            .approve(pool.address, INPUTS.lenderBDepositAmount);
+    return {
+      startTime,
+      pool,
+      lenderA,
+      lenderB,
+      mockUSDC,
+      poolManager,
+      borrower,
+      loan
+    };
+  }
 
-        // Create loanOne
-        const { loan } = await deployLoan(
-            pool.address,
-            borrower.address,
-            mockUSDC.address,
-            serviceConfiguration,
-            INPUTS.loan
-        );
-        // mint USDC for borrower to pay down loanOne
-        await mockUSDC.mint(borrower.address, INPUTS.loanPayment);
+  it("runs simulation", async () => {
+    const {
+      startTime,
+      pool,
+      lenderA,
+      lenderB,
+      mockUSDC,
+      poolManager,
+      borrower,
+      loan
+    } = await loadFixture(fixtures);
 
-        // Collateralize loan
-        await collateralizeLoan(loan, borrower, mockUSDC, 0);
+    // Initialization checks
+    // check that FL is zero
+    expect(await pool.firstLoss()).to.equal(0);
+    // Check that PM has no USDC balance
+    expect(await mockUSDC.balanceOf(poolManager.address)).to.equal(0);
 
-        return {
-            startTime,
-            pool,
-            lenderA,
-            lenderB,
-            mockUSDC,
-            poolManager,
-            borrower,
-            loan
-        };
-    }
+    // +2 days, lenderA deposits
+    await advanceToDay(startTime, 2);
+    await pool
+      .connect(lenderA)
+      .deposit(INPUTS.lenderADepositAmount, lenderA.address);
 
-    it("runs simulation", async () => {
-        const {
-            startTime,
-            pool,
-            lenderA,
-            lenderB,
-            mockUSDC,
-            poolManager,
-            borrower,
-            loan
-        } = await loadFixture(fixtures);
+    // sanity-check lenderA is cleared out
+    expect(await mockUSDC.balanceOf(lenderA.address)).to.equal(0);
+    // check pool tokens minted
+    expect(await pool.balanceOf(lenderA.address)).to.equal(
+      INPUTS.lenderADepositAmount
+    );
 
-        // Initialization checks
-        // check that FL is zero
-        expect(await pool.firstLoss()).to.equal(0);
-        // Check that PM has no USDC balance
-        expect(await mockUSDC.balanceOf(poolManager.address)).to.equal(0);
+    // +3 days, LenderB deposits
+    await advanceToDay(startTime, 3);
+    await pool
+      .connect(lenderB)
+      .deposit(INPUTS.lenderBDepositAmount, lenderB.address);
 
-        // +2 days, lenderA deposits
-        await advanceToDay(startTime, 2);
-        await pool
-            .connect(lenderA)
-            .deposit(INPUTS.lenderADepositAmount, lenderA.address);
+    // sanity-check lenderB is cleared out
+    expect(await mockUSDC.balanceOf(lenderB.address)).to.equal(0);
+    // check pool tokens minted
+    expect(await pool.balanceOf(lenderB.address)).to.equal(
+      INPUTS.lenderBDepositAmount
+    );
 
-        // sanity-check lenderA is cleared out
-        expect(await mockUSDC.balanceOf(lenderA.address)).to.equal(0);
-        // check pool tokens minted
-        expect(await pool.balanceOf(lenderA.address)).to.equal(
-            INPUTS.lenderADepositAmount
-        );
+    // +4  days, loan is funded
+    await advanceToDay(startTime, 4);
+    await fundLoan(loan, pool, poolManager);
+    await loan.connect(borrower).drawdown(INPUTS.loan.principal);
 
-        // +3 days, LenderB deposits
-        await advanceToDay(startTime, 3);
-        await pool
-            .connect(lenderB)
-            .deposit(INPUTS.lenderBDepositAmount, lenderB.address);
+    // +7 days, lenderA requests 200k PT redemption
+    await advanceToDay(startTime, 7);
+    await pool.crank(); // crank runs, but is meaningless
+    await pool.connect(lenderA).requestRedeem(200_000_000_000);
+    // check balances
+    expect(await pool.eligibleBalanceOf(lenderA.address)).to.equal(0);
+    expect(await pool.eligibleBalanceOf(lenderB.address)).to.equal(0);
+    expect(await pool.maxRedeem(lenderA.address)).to.equal(0);
+    expect(await pool.maxRedeem(lenderB.address)).to.equal(0);
+    expect(await pool.maxWithdraw(lenderA.address)).to.equal(0);
+    expect(await pool.maxWithdraw(lenderB.address)).to.equal(0);
 
-        // sanity-check lenderB is cleared out
-        expect(await mockUSDC.balanceOf(lenderB.address)).to.equal(0);
-        // check pool tokens minted
-        expect(await pool.balanceOf(lenderB.address)).to.equal(
-            INPUTS.lenderBDepositAmount
-        );
+    // +8 days, lenderB requests 300k PT redeption
+    await advanceToDay(startTime, 8);
+    await pool.connect(lenderB).requestRedeem(300_000_000_000);
 
-        // +4  days, loan is funded
-        await advanceToDay(startTime, 4);
-        await fundLoan(loan, pool, poolManager);
-        await loan.connect(borrower).drawdown(INPUTS.loan.principal);
+    // +11 days, first loan payment made
+    await advanceToDay(startTime, 11);
+    await mockUSDC.connect(borrower).approve(loan.address, INPUTS.loanPayment);
+    await loan.connect(borrower).completeNextPayment();
 
-        // +7 days, lenderA requests 200k PT redemption
-        await advanceToDay(startTime, 7);
-        await pool.crank(); // crank runs, but is meaningless
-        await pool.connect(lenderA).requestRedeem(200_000_000_000);
-        expect(await pool.eligibleBalanceOf(lenderA.address)).to.equal(0);
-        expect(await pool.eligibleBalanceOf(lenderB.address)).to.equal(0);
-        expect(await pool.maxRedeem(lenderA.address)).to.equal(0);
-        expect(await pool.maxRedeem(lenderB.address)).to.equal(0);
-        expect(await pool.maxWithdraw(lenderA.address)).to.equal(0);
-        expect(await pool.maxWithdraw(lenderB.address)).to.equal(0);
+    // +14 days, run the crank
+    await advanceToDay(startTime, 14);
+    await pool.crank();
+    // check balances
+    expect(await pool.maxRedeem(lenderA.address)).to.equal(283960890);
+    expect(await pool.maxRedeem(lenderB.address)).to.equal(425941335);
+    expect(await pool.maxWithdraw(lenderA.address)).to.equal(291666666);
+    expect(await pool.maxWithdraw(lenderB.address)).to.equal(437499999);
 
-        // +8 days, lenderB requests 300k PT redeption
-        await advanceToDay(startTime, 8);
-        await pool.connect(lenderB).requestRedeem(300_000_000_000);
+    // +18 days, complete payment made
+    await advanceToDay(startTime, 18);
+    await mockUSDC
+      .connect(borrower)
+      .approve(loan.address, INPUTS.loanPayment + INPUTS.loan.principal);
+    await loan.connect(borrower).completeFullPayment();
 
-        // +11 days, first loan payment made
-        await advanceToDay(startTime, 11);
-        await mockUSDC.connect(borrower).approve(loan.address, INPUTS.loanPayment);
-        await loan.connect(borrower).completeNextPayment();
+    // +21 days, run the crank
+    await advanceToDay(startTime, 21);
+    await pool.crank();
+    // check balances
+    expect(await pool.maxRedeem(lenderA.address)).to.equal(195141980444);
+    expect(await pool.maxRedeem(lenderB.address)).to.equal(292712970667);
+    expect(await pool.maxWithdraw(lenderA.address)).to.equal(200729166665);
+    expect(await pool.maxWithdraw(lenderB.address)).to.equal(301093749999);
 
-        // +14 days, run the crank
-        await advanceToDay(startTime, 14);
-        await pool.crank();
-        // check balances
-        expect(await pool.maxRedeem(lenderA.address)).to.equal(283960890);
-        expect(await pool.maxRedeem(lenderB.address)).to.equal(425941335);
-        expect(await pool.maxWithdraw(lenderA.address)).to.equal(291666666);
-        expect(await pool.maxWithdraw(lenderB.address)).to.equal(437499999);
+    // +22 days, lender A requests remaining PT redemption
+    await advanceToDay(startTime, 22);
+    await pool
+      .connect(lenderA)
+      .requestRedeem(await pool.maxRedeemRequest(lenderA.address));
 
-        // +18 days, complete payment made
-        await advanceToDay(startTime, 18);
-        await mockUSDC
-            .connect(borrower)
-            .approve(loan.address, INPUTS.loanPayment + INPUTS.loan.principal);
-        await loan.connect(borrower).completeFullPayment();
+    // +28 days, run the crank
+    await advanceToDay(startTime, 28);
+    await pool.crank();
+    // check balances
+    expect(await pool.maxRedeem(lenderA.address)).to.equal(425828499681);
+    expect(await pool.maxRedeem(lenderB.address)).to.equal(298694213967);
+    expect(await pool.maxWithdraw(lenderA.address)).to.equal(444944035136);
+    expect(await pool.maxWithdraw(lenderB.address)).to.equal(307425756527);
 
-        // +21 days, run the crank
-        await advanceToDay(startTime, 21);
-        await pool.crank();
-        // check balances
-        expect(await pool.maxRedeem(lenderA.address)).to.equal(195141980444);
-        expect(await pool.maxRedeem(lenderB.address)).to.equal(292712970667);
-        expect(await pool.maxWithdraw(lenderA.address)).to.equal(200729166665);
-        expect(await pool.maxWithdraw(lenderB.address)).to.equal(301093749999);
+    // +29 days, lender B requests remaining PT redemption
+    await advanceToDay(startTime, 29);
+    await pool
+      .connect(lenderB)
+      .requestRedeem(await pool.maxRedeemRequest(lenderB.address));
 
-        // +22 days, lender A requests remaining PT redemption
-        await advanceToDay(startTime, 22);
-        await pool
-            .connect(lenderA)
-            .requestRedeem(await pool.maxRedeemRequest(lenderA.address));
+    // +35 days, run the crank
+    await advanceToDay(startTime, 35);
+    await pool.crank();
+    // check balances
+    expect(await pool.maxRedeem(lenderA.address)).to.equal(451009487935);
+    expect(await pool.maxRedeem(lenderB.address)).to.equal(387442345077);
+    expect(await pool.maxWithdraw(lenderA.address)).to.equal(472632381854);
+    expect(await pool.maxWithdraw(lenderB.address)).to.equal(405010847309);
 
-        // +28 days, run the crank
-        await advanceToDay(startTime, 28);
-        await pool.crank();
-        // check balances
-        expect(await pool.maxRedeem(lenderA.address)).to.equal(425828499681);
-        expect(await pool.maxRedeem(lenderB.address)).to.equal(298694213967);
-        expect(await pool.maxWithdraw(lenderA.address)).to.equal(444944035136);
-        expect(await pool.maxWithdraw(lenderB.address)).to.equal(307425756527);
+    // + to close, crank, inspect results
+    await time.increaseTo((await pool.settings()).endDate);
+    await pool.crank();
 
-        // +29 days, lender B requests remaining PT redemption
-        await advanceToDay(startTime, 29);
-        await pool
-            .connect(lenderB)
-            .requestRedeem(await pool.maxRedeemRequest(lenderB.address));
+    // check balances
+    // Lender A and B can redeem the same # of shares, but their assets differ
+    // as they requested to withdraw at different times.
+    expect(await pool.maxRedeem(lenderA.address)).to.equal(476190476189);
+    expect(await pool.maxRedeem(lenderB.address)).to.equal(476190476189);
+    expect(await pool.maxWithdraw(lenderA.address)).to.equal(500320728572); // $500,320
+    expect(await pool.maxWithdraw(lenderB.address)).to.equal(502595938093); // $502,595
 
-        // +35 days, run the crank
-        await advanceToDay(startTime, 35);
-        await pool.crank();
-        // check balances
-        expect(await pool.maxRedeem(lenderA.address)).to.equal(451009487935);
-        expect(await pool.maxRedeem(lenderB.address)).to.equal(387442345077);
-        expect(await pool.maxWithdraw(lenderA.address)).to.equal(472632381854);
-        expect(await pool.maxWithdraw(lenderB.address)).to.equal(405010847309);
-
-        // + to close, crank, inspect results
-        await time.increaseTo((await pool.settings()).endDate);
-        await pool.crank();
-
-        // check balances
-        // Lender A and B can redeem the same # of shares, but their assets differ
-        // as they requested to withdraw at different times.
-        expect(await pool.maxRedeem(lenderA.address)).to.equal(476190476189);
-        expect(await pool.maxRedeem(lenderB.address)).to.equal(476190476189);
-        expect(await pool.maxWithdraw(lenderA.address)).to.equal(500320728572); // $500,320
-        expect(await pool.maxWithdraw(lenderB.address)).to.equal(502595938093); // $502,595
-
-        // Check that the pool only has dust
-        expect(await pool.totalAvailableAssets()).to.be.lt(5);
-        expect(await pool.totalAvailableSupply()).to.be.lt(5);
-    });
+    // Check that the pool only has dust
+    expect(await pool.totalAvailableAssets()).to.be.lt(5);
+    expect(await pool.totalAvailableSupply()).to.be.lt(5);
+  });
 });
