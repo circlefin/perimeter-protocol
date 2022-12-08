668cea17f141c0b2192a7e76ede0872bad9e64f2
Adrian Soghoian <adrian.soghoian@contractor.circle.com>
2022-12-08 13:48:13 -0500

    Lint

--- test/upgrades/DeployerUUPSUpgradeable.test.ts
+++ test/upgrades/DeployerUUPSUpgradeable.test.ts
@@ -5,61 +5,61 @@ import { deployServiceConfiguration } from "../support/serviceconfiguration";
 import { getCommonSigners } from "../support/utils";
 
 describe("DeployerUUPSUpgradeable", () => {
-    async function deployFixture() {
-        const { deployer, other } = await getCommonSigners();
+  async function deployFixture() {
+    const { deployer, other } = await getCommonSigners();
 
-        const { serviceConfiguration } = await deployServiceConfiguration();
+    const { serviceConfiguration } = await deployServiceConfiguration();
 
-        const DeployerUUPSUpgradeableMock = await ethers.getContractFactory(
-            "DeployerUUPSUpgradeableMock",
-            deployer
-        );
-        const deployerUUPSUpgradeableMock = await upgrades.deployProxy(
-            DeployerUUPSUpgradeableMock,
-            [serviceConfiguration.address],
-            { kind: "uups" }
-        );
-        await deployerUUPSUpgradeableMock.deployed();
+    const DeployerUUPSUpgradeableMock = await ethers.getContractFactory(
+      "DeployerUUPSUpgradeableMock",
+      deployer
+    );
+    const deployerUUPSUpgradeableMock = await upgrades.deployProxy(
+      DeployerUUPSUpgradeableMock,
+      [serviceConfiguration.address],
+      { kind: "uups" }
+    );
+    await deployerUUPSUpgradeableMock.deployed();
 
-        return {
-            deployerUUPSUpgradeableMock,
-            deployer,
-            other
-        };
-    }
+    return {
+      deployerUUPSUpgradeableMock,
+      deployer,
+      other
+    };
+  }
 
-    describe("Upgrades", () => {
-        it("can be upgraded", async () => {
-            const { deployerUUPSUpgradeableMock, deployer } = await loadFixture(
-                deployFixture
-            );
+  describe("Upgrades", () => {
+    it("can be upgraded", async () => {
+      const { deployerUUPSUpgradeableMock, deployer } = await loadFixture(
+        deployFixture
+      );
 
-            const V2 = await ethers.getContractFactory(
-                "DeployerUUPSUpgradeableMockV2",
-                deployer
-            );
-            await upgrades.upgradeProxy(deployerUUPSUpgradeableMock.address, V2);
+      const V2 = await ethers.getContractFactory(
+        "DeployerUUPSUpgradeableMockV2",
+        deployer
+      );
+      await upgrades.upgradeProxy(deployerUUPSUpgradeableMock.address, V2);
 
-            const v2 = await ethers.getContractAt(
-                "DeployerUUPSUpgradeableMockV2",
-                deployerUUPSUpgradeableMock.address
-            );
-            expect(await v2.foo()).to.equal("baz");
-        });
+      const v2 = await ethers.getContractAt(
+        "DeployerUUPSUpgradeableMockV2",
+        deployerUUPSUpgradeableMock.address
+      );
+      expect(await v2.foo()).to.equal("baz");
     });
+  });
 
-    describe("Permissions", () => {
-        it("only deployer can upgrade", async () => {
-            const { deployerUUPSUpgradeableMock, deployer, other } =
-                await loadFixture(deployFixture);
+  describe("Permissions", () => {
+    it("only deployer can upgrade", async () => {
+      const { deployerUUPSUpgradeableMock, deployer, other } =
+        await loadFixture(deployFixture);
 
-            const V2 = await ethers.getContractFactory(
-                "DeployerUUPSUpgradeableMockV2",
-                other
-            );
-            await expect(
-                upgrades.upgradeProxy(deployerUUPSUpgradeableMock.address, V2)
-            ).to.be.revertedWith("Upgrade: unauthorized");
-        });
+      const V2 = await ethers.getContractFactory(
+        "DeployerUUPSUpgradeableMockV2",
+        other
+      );
+      await expect(
+        upgrades.upgradeProxy(deployerUUPSUpgradeableMock.address, V2)
+      ).to.be.revertedWith("Upgrade: unauthorized");
     });
+  });
 });
