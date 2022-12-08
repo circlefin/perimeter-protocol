1a207e7d970431f7bb275b89c5651284e58d74a5
pickle4325 <114615432+pickle4325@users.noreply.github.com>
2022-12-08 14:12:17 -0500

    Change SPDX-License-Identifiers to MIT (#141)
    * Change SPDX-License-Identifier to MIT
    
    * Change SPDX-License-Identifier to MIT

--- contracts/libraries/LoanLib.sol
+++ contracts/libraries/LoanLib.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
 import {ERC20PresetMinterPauser} from "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
