d1b28f369170932e387a4882978d867dfc016214
Brice Stacey <brice@m2.xyz>
2022-12-08 15:04:35 -0500

    Update permissionless deploy script

--- scripts/deploy-permissionless.ts
+++ scripts/deploy-permissionless.ts
@@ -1,5 +1,6 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 import hre from "hardhat";
+import { getCommonSigners } from "../test/support/utils";
 
 async function main() {
   // The token we use for the liquidity asset must exist. If it is not defined, we'll deploy a mock token.
@@ -12,94 +13,153 @@ async function main() {
     usdcAddress = usdc.address;
   }
 
+  const { admin, operator, deployer, pauser } = await getCommonSigners();
+
   // Deploy ServiceConfiguration
   const ServiceConfiguration = await ethers.getContractFactory(
-    "ServiceConfiguration"
+    "ServiceConfiguration",
+    admin
   );
-  const serviceConfiguration = await ServiceConfiguration.deploy();
+  const serviceConfiguration = await upgrades.deployProxy(
+    ServiceConfiguration,
+    { kind: "uups" }
+  );
   await serviceConfiguration.deployed();
-
   console.log(
     `ServiceConfiguration deployed to ${serviceConfiguration.address}`
   );
 
-  // Set USDC as a liquidity asset for the protocol
-  await serviceConfiguration.setLiquidityAsset(usdcAddress, true);
-  console.log(`Updated ServiceConfiguration to add USDC as a liquidity asset`);
+  // Grant operator role
+  await serviceConfiguration
+    .connect(admin)
+    .grantRole(serviceConfiguration.OPERATOR_ROLE(), operator.address);
+  console.log(`Granted operator role to ${operator.address}`);
+
+  // Grant pauser role
+  await serviceConfiguration
+    .connect(admin)
+    .grantRole(serviceConfiguration.PAUSER_ROLE(), pauser.address);
+  console.log(`Granted pauser role to ${pauser.address}`);
+
+  // Grant deployer role
+  await serviceConfiguration
+    .connect(admin)
+    .grantRole(serviceConfiguration.DEPLOYER_ROLE(), deployer.address);
+  console.log(`Granted deployer role to ${deployer.address}`);
 
   // Deploy PoolLib
   const PoolLib = await ethers.getContractFactory("PoolLib");
   const poolLib = await PoolLib.deploy();
-
+  await poolLib.deployed();
   console.log(`PoolLib deployed to ${poolLib.address}`);
 
   // Deploy LoanLib
   const LoanLib = await ethers.getContractFactory("LoanLib");
   const loanLib = await LoanLib.deploy();
-
+  await loanLib.deployed();
   console.log(`LoanLib deployed to ${loanLib.address}`);
 
   // Deploy WithdrawControllerFactory
   const WithdrawControllerFactory = await ethers.getContractFactory(
-    "WithdrawControllerFactory",
-    {
-      libraries: {
-        PoolLib: poolLib.address
-      }
-    }
+    "WithdrawControllerFactory"
   );
   const withdrawControllerFactory = await WithdrawControllerFactory.deploy(
     serviceConfiguration.address
   );
   await withdrawControllerFactory.deployed();
-
   console.log(
     `WithdrawControllerFactory deployed to ${withdrawControllerFactory.address}`
   );
-
-  // Deploy PoolControllerFactory
-  const PoolControllerFactory = await ethers.getContractFactory(
-    "PoolControllerFactory",
+  const WithdrawController = await ethers.getContractFactory(
+    "WithdrawController",
     {
       libraries: {
         PoolLib: poolLib.address
       }
     }
   );
+  const withdrawController = await WithdrawController.deploy();
+  await withdrawController.deployed();
+  console.log(`WithdrawController deployed to ${withdrawController.address}`);
+  await withdrawControllerFactory
+    .connect(deployer)
+    .setImplementation(withdrawController.address);
+  console.log(`WithdrawController set as implementation for its factory`);
+
+  // Deploy PoolControllerFactory
+  const PoolControllerFactory = await ethers.getContractFactory(
+    "PoolControllerFactory"
+  );
   const poolControllerFactory = await PoolControllerFactory.deploy(
     serviceConfiguration.address
   );
   await poolControllerFactory.deployed();
-
   console.log(
     `PoolControllerFactory deployed to ${poolControllerFactory.address}`
   );
-
-  // Deploy PoolFactory
-  const PoolFactory = await ethers.getContractFactory("PoolFactory", {
+  const PoolController = await ethers.getContractFactory("PoolController", {
     libraries: {
       PoolLib: poolLib.address
     }
   });
+  const poolController = await PoolController.deploy();
+  await poolController.deployed();
+  console.log(`PoolController deployed to ${poolController.address}`);
+  await poolControllerFactory
+    .connect(deployer)
+    .setImplementation(poolController.address);
+  console.log(`PoolController set as implementation for its factory`);
+
+  // Deploy PoolFactory
+  const PoolFactory = await ethers.getContractFactory("PoolFactory", {});
   const poolFactory = await PoolFactory.deploy(
-    usdcAddress,
+    serviceConfiguration.address,
     withdrawControllerFactory.address,
     poolControllerFactory.address
   );
   await poolFactory.deployed();
-
   console.log(`PoolFactory deployed to ${poolFactory.address}`);
+  const Pool = await ethers.getContractFactory("Pool", {
+    libraries: {
+      PoolLib: poolLib.address
+    }
+  });
+  const pool = await Pool.deploy();
+  await pool.deployed();
+  console.log(`Pool deployed to ${pool.address}`);
+  await poolFactory.connect(deployer).setImplementation(pool.address);
+  console.log(`Pool set as imlementation for its factory`);
 
   // Deploy LoanFactory
-  const LoanFactory = await ethers.getContractFactory("LoanFactory", {
+  const LoanFactory = await ethers.getContractFactory("LoanFactory");
+  const loanFactory = await LoanFactory.deploy(serviceConfiguration.address);
+  await loanFactory.deployed();
+  console.log(`LoanFactory deployed to ${loanFactory.address}`);
+
+  const Loan = await ethers.getContractFactory("Loan", {
     libraries: {
       LoanLib: loanLib.address
     }
   });
-  const loanFactory = await LoanFactory.deploy(serviceConfiguration.address);
-  await loanFactory.deployed();
+  const loan = await Loan.deploy();
+  await loan.deployed();
+  console.log(`Loan deployed to ${loan.address}`);
+  await loanFactory.connect(deployer).setImplementation(loan.address);
+  console.log(`Loan set as implementation for its Factory`);
 
-  console.log(`LoanFactory deployed to ${loanFactory.address}`);
+  // Setup LoanFactory
+  const tx1 = await serviceConfiguration
+    .connect(operator)
+    .setLoanFactory(loanFactory.address, true);
+  await tx1.wait();
+  console.log(`ServiceConfiguration: set LoanFactory as valid`);
+
+  // Set USDC as a liquidity asset for the protocol
+  const tx2 = await serviceConfiguration
+    .connect(operator)
+    .setLiquidityAsset(usdcAddress, true);
+  await tx2.wait();
+  console.log(`ServiceConfiguration: set USDC as a liquidity asset`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
