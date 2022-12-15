1ba0e778ba323b919037fe5e076ba699e582dfe9
Brice Stacey <brice@m2.xyz>
2022-12-15 14:59:30 -0500

    Add multichain support on polygon

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -63,7 +63,12 @@ const config: HardhatUserConfig | ExtendedHardhatUserConfig = {
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY!,
+      goerli: process.env.ETHERSCAN_API_KEY!,
+      polygon: process.env.POLYGONSCAN_API_KEY!,
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY!
+    }
   }
 };
 
